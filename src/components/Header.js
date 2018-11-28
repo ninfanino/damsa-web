4665926668e1c975c6b7988c90edebfbfbaecb3c
ninfan <ninfa_sys@hotmail.com>
2018-11-28 12:03:27 -0600

    Header responsive

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
-
+import $ from 'jquery';
 
 
 
 export default class Header extends Component {
+	
+	openMenu(){
+		$('.menu-mobile').toggleClass('open');
+		$('.navbar-toggle').toggleClass('open');
+	}
+
 	render () {
 		return (
 			<header className="header">
@@ -11,8 +17,8 @@ export default class Header extends Component {
 					<div className="row">
 						<div className="col-md-12 relative" id="menu1">
 
-							<nav id="main">
-								<a ><span>atencionaclientes@damsa.com.m</span></a>
+							<nav className="main">
+								<a ><span>atencionaclientes@damsa.com.mx</span></a>
 								<a ><span>01 800 7013959</span></a>
 								<a ><span>EN</span></a>
 								<a className="border-none"><span className="icon-facebook" ></span></a>
@@ -20,21 +26,45 @@ export default class Header extends Component {
 			          			<a className="border-none" ><span className="icon-linkedin" ></span></a>
 							</nav>
 						</div>
+						
+						<div className="col-md-12">
+							
+  								<div class="container-fluid">
+					        			<button type="button" className="navbar-toggle" onClick={this.openMenu}>
+        									<span class="icon-bar"></span>
+        									<span class="icon-bar"></span>
+        									<span class="icon-bar"></span>
+      									</button>
 
-			        	<div className="col-md-12 relative" id="menu">
-			       			<img className="logodam" src="../images/damsa.png"  />
-			        		<nav id="main">
-					          <a ><span>NOSOTROS</span></a>
-					          <a ><span>SERVICIOS</span></a>
-					          <a ><span>TALENTO</span></a>
-					          <a ><span>ESR</span></a>
-					          <a ><span>GALERIA</span></a>
-					          <a ><span>DAMSACLUB</span></a>
-					          <a ><span>BLOG</span></a>
-					          <a ><span>CONTACTO</span></a>
-					          <a ><span className="icon-user" ></span></a>
-					        </nav>
-			        	</div>
+      									<img className="logodam" src="../images/damsa.png"  />
+
+      									<nav id="menu-complete" className="main">
+								          <a ><span>NOSOTROS</span></a>
+								          <a ><span>SERVICIOS</span></a>
+								          <a ><span>TALENTO</span></a>
+								          <a ><span>ESR</span></a>
+								          <a ><span>GALERIA</span></a>
+								          <a ><span>DAMSACLUB</span></a>
+								          <a ><span>BLOG</span></a>
+								          <a ><span>CONTACTO</span></a>
+								          <a ><span className="icon-user" ></span></a>
+								        </nav>
+
+								        <div className="menu-mobile">
+								          <a ><span>NOSOTROS</span></a>
+								          <a ><span>SERVICIOS</span></a>
+								          <a ><span>TALENTO</span></a>
+								          <a ><span>ESR</span></a>
+								          <a ><span>GALERIA</span></a>
+								          <a ><span>DAMSACLUB</span></a>
+								          <a ><span>BLOG</span></a>
+								          <a ><span>CONTACTO</span></a>
+								          <a ><span className="icon-user" ></span></a>
+					        			</div>
+    								
+    							</div>
+					    	
+					    </div>
 					</div>
 		    	</div>
 		    </header>
